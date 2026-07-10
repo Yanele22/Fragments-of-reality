292be629789b1a6403dc614444ef097ffa94d7d3
Boitumelo Charles Rachoshi <boracjhb025@student.wethinkcode.co.za>
2026-07-10 23:08:07 +0200

    feat(ui): add DialogueBox container with typewriter integration

--- core/src/main/java/com/fragmentsofreality/ui/DialogueBox.java
+++ core/src/main/java/com/fragmentsofreality/ui/DialogueBox.java
@@ -1,4 +1,41 @@
 package com.fragmentsofreality.ui;
 
-public class DialogueBox {
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
+
+public class DialogueBox extends Table {
+    private TypeWriterLabel textLabel;
+    private Skin skin;
+
+    public DialogueBox(Skin skin) {
+        super(skin);
+        this.skin = skin;
+
+        this.setBackground("default-pane");
+
+        textLabel = new TypeWriterLabel("", skin);
+        textLabel.setAlignment(Align.topLeft);
+
+        this.add(textLabel).expand().fill().pad(20f);
+
+        this.setVisible(false);
+    }
+
+    public void showDialogue(String text) {
+        this.setVisible(true);
+        textLabel.animateText(text);
+    }
+
+    public void hideDialogue() {
+        this.setVisible(false);
+    }
+
+    public boolean isTextFinished() {
+        return textLabel.isAnimationFinished();
+    }
+
+    public void skipText() {
+        textLabel.skipAnimation();
+    }
 }
