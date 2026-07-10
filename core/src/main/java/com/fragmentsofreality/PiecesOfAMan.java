bfd6eb7000ca1140ac7f618fe8304c260a6b1040
Boitumelo Charles Rachoshi <boracjhb025@student.wethinkcode.co.za>
2026-07-11 00:07:23 +0200

    fix: resolve build gradle pathing and configure sandbox UI screens

--- core/src/main/java/com/fragmentsofreality/PiecesOfAMan.java
+++ core/src/main/java/com/fragmentsofreality/PiecesOfAMan.java
@@ -1,2 +1,17 @@
 package com.fragmentsofreality;
-public class PiecesOfAMan {}
+
+import com.badlogic.gdx.Game;
+import com.fragmentsofreality.screens.MenuScreen;
+
+public class PiecesOfAMan extends Game {
+
+    @Override
+    public void create() {
+        this.setScreen(new MenuScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+}
--- core/src/main/java/com/fragmentsofreality/screens/MenuScreen.java
+++ core/src/main/java/com/fragmentsofreality/screens/MenuScreen.java
@@ -1,4 +1,81 @@
 package com.fragmentsofreality.screens;
 
-public class MenuScreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.fragmentsofreality.PiecesOfAMan;
+import com.fragmentsofreality.ui.DialogueBox;
+
+public class MenuScreen extends ScreenAdapter {
+    private final PiecesOfAMan game;
+    private Stage stage;
+    private Skin skin;
+    private DialogueBox testDialogue;
+
+    public MenuScreen(PiecesOfAMan game) {
+        this.game = game;
+        this.stage = new Stage(new ScreenViewport());
+        this.skin = new Skin(Gdx.files.internal("assets/ui/uiskin.json"));
+
+        setupUI();
+    }
+
+    private void setupUI() {
+        Table rootTable = new Table();
+        rootTable.setFillParent(true);
+
+        Label titleLabel = new Label("Pieces of a Man", skin, "title");
+        titleLabel.setAlignment(Align.center);
+
+        Label promptLabel = new Label("Press SPACE to Awaken", skin);
+        promptLabel.setAlignment(Align.center);
+
+        rootTable.add(titleLabel).expandX().padBottom(50f).row();
+        rootTable.add(promptLabel).expandX();
+
+        stage.addActor(rootTable);
+
+        testDialogue = new DialogueBox(skin);
+        testDialogue.setWidth(Gdx.graphics.getWidth());
+        testDialogue.setHeight(150f);
+        testDialogue.setPosition(0, 0);
+
+        stage.addActor(testDialogue);
+
+        testDialogue.showDialogue("This is a test of the typewriter effect. Focus your intentional attention here. The Phantoms are waiting...");
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0.05f, 0.05f, 0.05f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+        if (testDialogue != null) {
+            testDialogue.setWidth(width);
+        }
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+        skin.dispose();
+    }
 }
