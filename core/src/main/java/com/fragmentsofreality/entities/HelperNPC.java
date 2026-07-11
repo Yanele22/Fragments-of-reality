a9f7810ab196b34cb50f35cde87db23999d81c94
Code Whisperer <120354783+Infrixxx@users.noreply.github.com>
2026-07-11 08:51:32 +0000

    feat: define NPC interaction boundaries and state flags

--- core/src/main/java/com/fragmentsofreality/entities/HelperNPC.java
+++ core/src/main/java/com/fragmentsofreality/entities/HelperNPC.java
@@ -1,4 +1,39 @@
 package com.fragmentsofreality.entities;
 
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
 public class HelperNPC {
-}
+    public Vector2 position;
+        public Rectangle bounds;
+            public boolean hasGivenClue = false;
+
+                public HelperNPC(float x, float y) {
+                        this.position = new Vector2(x, y);
+                                this.bounds = new Rectangle(x, y, 32, 32); // Interaction radius
+                                    }
+
+                                        public boolean canInteract(Rectangle playerBounds) {
+                                                return this.bounds.overlaps(playerBounds) && !hasGivenClue;
+                                                    }
+                                                    }
+                                                    package com.fragmentsofreality.entities;
+
+                                                    import com.badlogic.gdx.math.Rectangle;
+                                                    import com.badlogic.gdx.math.Vector2;
+
+                                                    public class HelperNPC {
+                                                        public Vector2 position;
+                                                            public Rectangle bounds;
+                                                                public boolean hasGivenClue = false;
+
+                                                                    public HelperNPC(float x, float y) {
+                                                                            this.position = new Vector2(x, y);
+                                                                                    this.bounds = new Rectangle(x, y, 32, 32); // Interaction radius
+                                                                                        }
+
+                                                                                            public boolean canInteract(Rectangle playerBounds) {
+                                                                                                    return this.bounds.overlaps(playerBounds) && !hasGivenClue;
+                                                                                                        }
+                                                                                                        }
+                                                                                                        
