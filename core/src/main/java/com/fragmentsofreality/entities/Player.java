052edf58553d280057cdc983b1a9536c64fbbc47
Neo Phukubye <nephujhb025@student.wethinkcode.co.za>
2026-07-11 12:32:34 +0200

    implementation

--- core/src/main/java/com/fragmentsofreality/entities/Player.java
+++ core/src/main/java/com/fragmentsofreality/entities/Player.java
@@ -5,41 +5,18 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Player {
     public Vector2 position;
-        public Rectangle bounds;
-            public float speed = 200f;
+    public Rectangle bounds;
+    public float speed = 200f;
 
-                public Player(float startX, float startY) {
-                        this.position = new Vector2(startX, startY);
-                                this.bounds = new Rectangle(startX, startY, 32, 32);
-                                    }
+    public Player(float startX, float startY) {
+        this.position = new Vector2(startX, startY);
+        this.bounds = new Rectangle(startX, startY, 32, 32);
+    }
 
-                                        public void update(float delta, float dirX, float dirY) {
-                                                // Move position based on input vectors
-                                                        position.add(dirX * speed * delta, dirY * speed * delta);
-                                                                // Sync collision box to new position
-                                                                        bounds.setPosition(position.x, position.y);
-                                                                            }
-                                                                            }
-                                                                            package com.fragmentsofreality.entities;
-
-                                                                            import com.badlogic.gdx.math.Rectangle;
-                                                                            import com.badlogic.gdx.math.Vector2;
-
-                                                                            public class Player {
-                                                                                public Vector2 position;
-                                                                                    public Rectangle bounds;
-                                                                                        public float speed = 200f;
-
-                                                                                            public Player(float startX, float startY) {
-                                                                                                    this.position = new Vector2(startX, startY);
-                                                                                                            this.bounds = new Rectangle(startX, startY, 32, 32);
-                                                                                                                }
-
-                                                                                                                    public void update(float delta, float dirX, float dirY) {
-                                                                                                                            // Move position based on input vectors
-                                                                                                                                    position.add(dirX * speed * delta, dirY * speed * delta);
-                                                                                                                                            // Sync collision box to new position
-                                                                                                                                                    bounds.setPosition(position.x, position.y);
-                                                                                                                                                        }
-                                                                                                                                                        }
-                                                                                                                                                        
+    public void update(float delta, float dirX, float dirY) {
+        // Move position based on input vectors
+        position.add(dirX * speed * delta, dirY * speed * delta);
+        // Sync collision box to new position
+        bounds.setPosition(position.x, position.y);
+    }
+}
--- core/src/main/java/com/fragmentsofreality/utils/AssetLoader.java
+++ core/src/main/java/com/fragmentsofreality/utils/AssetLoader.java
@@ -1,4 +1,30 @@
 package com.fragmentsofreality.utils;
 
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.audio.Music;
+
 public class AssetLoader {
+    public final AssetManager manager = new AssetManager();
+
+    // Textures
+    public static final String PLAYER = "textures/player.png";
+    public static final String NPC_HELPER = "textures/npc_helper.png";
+    public static final String NPC_DECEIVER = "textures/npc_deceiver.png";
+    public static final String PHANTOM = "textures/phantom.png";
+    public static final String FRAGMENT = "textures/fragment.png";
+
+    public void load() {
+        // In a real project, these files would need to exist in the assets folder
+        // manager.load(PLAYER, Texture.class);
+        // manager.load(NPC_HELPER, Texture.class);
+        // manager.load(NPC_DECEIVER, Texture.class);
+        // manager.load(PHANTOM, Texture.class);
+        // manager.load(FRAGMENT, Texture.class);
+    }
+
+    public void dispose() {
+        manager.dispose();
+    }
 }
